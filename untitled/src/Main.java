3f85b6f77924c63be2ace8bb87d378b94a7c1e1f
hinesa18 <hineska6@gmail.com>
2024-10-01 09:24:48 -0400

    Update Main.java

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -13,25 +13,25 @@ public class Main {
 		int var1 = Integer.parseInt(input.nextLine());
 		//write an if statement that prints "hello world" if the input was less than 4
 		if (var1<4);
-            System.out.println("hello world");
+        System.out.println("hello world");
 		//write an if statement that prints out "Why did the man fall down the well?" if the input was less than 5
-		if (var1);
-            System.out.println("Why did the man fall down the well?");
+		if (var1<5);
+        System.out.println("Why did the man fall down the well?");
 		//write an if statement that prints out "Because he couldn’t see that well." if the input was greater than 1
 		if (var1>1);
-            System.out.println("Because he couldn't see that well.");
+        System.out.println("Because he couldn't see that well.");
 		//write an if statement that prints out "When does a joke become a dad joke?"  if the input was equal to 8
 		if (var1 == 8);
-            System.out.println("When does a joke become a dad joke?");
+        System.out.println("When does a joke become a dad joke?");
 		//write an if statement that prints out "When it becomes apparent."  if the input was greater than or equal to 8
 		if (var1 >= 8);
-            System.out.println("When it becomes apparent.");
+        System.out.println("When it becomes apparent.");
 		//write an if statement that prints out "I used to hate facial hair," if the input is less than or equal to 12
 		if (var1 <= 12);
-            System.out.println("I used to hate facial hair,");
+        System.out.println("I used to hate facial hair,");
 		//write an if statement that prints out " but then it grew on me."  if the input does not equal 23
         if (!(var1 == 23));
-            System.out.println(" but then it grew on me.");
+        System.out.println(" but then it grew on me.");
 
     }
 
